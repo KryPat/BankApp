bbed5866bc7279bd45a68d94e0b889d348cca25a
KryPat <patryk14901@gmail.com>
2022-08-25 23:16:06 +0200

    Added PersistentBankAccount

---
+++ src/main/scala/com/KryPat/actors/PersistentBankAccount.scala
@@ -0,0 +1,57 @@
+package com.KryPat.actors
+
+import akka.actor.typed.ActorRef
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import akka.remote.transport.TestTransport.Behavior
+
+// a single bank account
+class PersistentBankAccount {
+
+  /*
+    - fault tolerance
+    - auditing
+   */
+  // IMPORTANT! Never use Double for money in real life application
+  // commands = messages
+  sealed trait Command
+
+  case class CreateBankAccount(user: String, currency: String, initialBalance: Double, replyTo: ActorRef[Response]) extends Command
+  case class UpdateBalance(id: String, currency: String, amount: Double, /* can be < 0 */ replyTo: ActorRef[Response]) extends Command
+  case class GetBankAccount(id: String, replyTo: ActorRef[Response]) extends Command
+
+  // events = to persist to Cassandra
+  trait Event
+  case class BankAccountCreated(bankAccount: BankAccount) extends Event
+  case class BalanceUpdated(amount: Double) extends Event
+
+  // state
+  case class BankAccount(id: String, user: String, currency: String, balance: Double)
+
+  // responses
+  sealed trait Response
+  case class BankAccountCreatedResponse(id: String) extends Response
+  case class BankAccountBalanceUpdatedResponse(maybeBankAccount: Option[BankAccount]) extends Response
+  case class GetBankAccountResponse(maybeBankAccount: Option[BankAccount]) extends  Response
+
+  // command handler = message handler => persist and event
+  // event handler => update state
+  // state
+
+  val commandHandler: (BankAccount, Command) => Effect[Event, BankAccount] = (state, command) =>
+    command match {
+      case CreateBankAccount(user, currency, initialBalance, replyTo) =>
+        val id = state.id
+        Effect
+          .persist(BankAccountCreated(BankAccount(id, user, currency, initialBalance)))
+          .thenReply(replyTo)(_ => BankAccountCreatedResponse(id))
+    }
+  val eventHandler: (BankAccount, Event) => BankAccount = ???
+  def apply(id: String): Behavior[Command]
+    EventSourcedBehavior[Command, Event, BankAccount](
+      persistenceId = PersistenceId.ofUniqueId(id),
+      emptyState = BankAccount(id, "", "", 0.0), //unused
+      commandHandler = commandHandler,
+      eventHandler = eventHandler
+    )
+}
